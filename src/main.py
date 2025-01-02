07f6cddc36610082dfce57c3195bbb99e1d467f3
carsondecker <carsondecker1205@gmail.com>
2025-01-02 14:37:01 -0600

    Added copying static to public

--- src/main.py
+++ src/main.py
@@ -1,7 +1,29 @@
+import os
+import shutil
 from textnode import *
 
 def main():
-    print(TextNode("This is a text node", TextType.BOLD))
+    copy_static_to_public()
+
+def copy_static_to_public():
+    project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    start_path = os.path.join(project_dir, "static")
+    dest_path = os.path.join(project_dir, "public")
+    if os.path.exists(dest_path):
+        shutil.rmtree(dest_path)
+    os.mkdir(dest_path)
+    recursively_copy_tree(start_path, dest_path)
+    
+def recursively_copy_tree(curr_path, dest_path):
+    dir_list = os.listdir(curr_path)
+    for item in dir_list:
+        item_path = os.path.join(curr_path, item)
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, dest_path)
+        else:
+            new_dest_path = os.path.join(dest_path, item)
+            os.mkdir(new_dest_path)
+            recursively_copy_tree(item_path, new_dest_path)
 
 if __name__ == "__main__":
     main()
